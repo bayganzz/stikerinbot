7fb1f926738237a4bdfacddf4d318f590503f2c1
Bay ~ <87753667+bayganzz@users.noreply.github.com>
2021-11-26 22:40:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 let fs = require('fs')
-global.owner = ['6285775458606', '6288709645535'] // Letakan nomor kamu disini
+global.owner = ['6285775458606', '6287820239077'] // Letakan nomor kamu disini
 global.mods = [] // Moderator?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Pengguna premium tidak memerlukan limit
 global.APIs = { // API Prefix
