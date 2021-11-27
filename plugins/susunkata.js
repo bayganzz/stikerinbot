c7798c27fef953130022d39422a248a7c7c5b126
Bay ~ <87753667+bayganzz@users.noreply.github.com>
2021-11-27 08:26:26 +0700

    Update susunkata.js

--- plugins/susunkata.js
+++ plugins/susunkata.js
@@ -26,7 +26,7 @@ Bonus: ${poin} XP
         await conn.sendButton(m.chat, caption, '© stikerin', 'Bantuan', '.suka'),
         json, poin,
         setTimeout(async () => {
-            if (conn.susunkata[id]) await conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, '© stikerin', 'Susun Kata', '.susunkata')
+            if (conn.susunkata[id]) await conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, '© Bay', 'Susun Kata', '.susunkata')
             delete conn.susunkata[id]
         }, timeout)
     ]
